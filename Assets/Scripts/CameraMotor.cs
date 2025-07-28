319761187760f206b33fbdfdd4e32dc9a3dc3e87
Jamie Codes <163902855+Jamie-CodesYT@users.noreply.github.com>
2025-07-28 18:55:09 +0100

    Initial Commit

---
+++ Assets/Scripts/CameraMotor.cs
@@ -0,0 +1,47 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CameraMotor : MonoBehaviour
+{
+    public Transform lookAt;
+    public float boundX = 0.15f;
+    public float boundY = 0.05f;
+
+    public float smoothTime = 0.2f;
+    private Vector3 velocity = Vector3.zero;
+
+    private void LateUpdate()
+    {
+        Vector3 delta = Vector3.zero;
+
+        // Check if inside bounds on x axis
+        float deltaX = lookAt.position.x - transform.position.x;
+        if (deltaX > boundX || deltaX < -boundX)
+        {
+            if (transform.position.x < lookAt.position.x)
+            {
+                delta.x = deltaX - boundX;
+            }
+            else
+            {
+                delta.x = deltaX + boundX;
+            }
+        }
+        // Check if inside bounds on x axis
+        float deltaY = lookAt.position.y - transform.position.y;
+        if (deltaY > boundY || deltaY < -boundY)
+        {
+            if (transform.position.y < lookAt.position.y)
+            {
+                delta.y = deltaY - boundY;
+            }
+            else
+            {
+                delta.y = deltaY + boundY;
+            }
+        }
+
+        Vector3 targetPosition = transform.position + new Vector3(delta.x, delta.y, 0);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Sprite[] Sprites; // Index 0 = left, 1 = up, 2 = right, 3 = down
+    public float moveSpeed = 2f;
+    private Vector2 movement;
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if (movement.x > 0)
+        {
+            spriteRenderer.sprite = Sprites[2];
+        } else if (movement.x < 0)
+        {
+            spriteRenderer.sprite = Sprites[0];
+        }
+        if (movement.y > 0)
+        {
+            spriteRenderer.sprite = Sprites[1];
+        } else if (movement.y < 0)
+        {
+            spriteRenderer.sprite = Sprites[3];
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + (movement.normalized * moveSpeed * Time.deltaTime));
+    }
+}
